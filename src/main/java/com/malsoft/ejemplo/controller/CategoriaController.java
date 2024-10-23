658985c0403e75ced50c32664ba594d698c4cfc5
MAL501 <marcosaragonlopez@gmail.com>
2024-10-23 17:03:09 +0200

    Añadimos fotos

--- src/main/java/com/malsoft/ejemplo/controller/CategoriaController.java
+++ src/main/java/com/malsoft/ejemplo/controller/CategoriaController.java
@@ -6,10 +6,14 @@ import com.malsoft.ejemplo.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -32,5 +36,25 @@ public class CategoriaController {
     public String borrarCategoria(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
         categoriaRepository.deleteById(id);
         return "redirect:/categorias";  // Redirige de nuevo a la lista de categorías
+    }
+    @GetMapping("/categorias/new")
+    public String crearCategoria(Model model) {
+        model.addAttribute("categoria",new Categoria());
+        return "categoria-new";
+    }
+    @PostMapping("/categorias/new")
+    public String guardarCategoria( @ModelAttribute("categoria") Categoria categoria,Model model,
+                                    @RequestAttribute("file") MultipartFile file) {
+        Path ruta = Paths.get("uploads\\"+file.getOriginalFilename());
+        try {
+            byte[] contenido = file.getBytes();
+            Files.write(ruta,contenido);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        categoria.setFoto(file.getOriginalFilename());
+        categoriaRepository.save(categoria);
+        return "redirect:/categorias";
+
     }
 }
--- src/main/java/com/malsoft/ejemplo/entity/Categoria.java
+++ src/main/java/com/malsoft/ejemplo/entity/Categoria.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @Data   //Incluye @ToString, @Getter, @Setter, @RequiredArgsConstructor y @EqualsAndHashCode
 //@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder    //Patron Builder
 //Anotacion de Spring Data JPA
 @Entity
@@ -28,5 +30,6 @@ public class Categoria {
     @OneToMany(targetEntity = Producto.class, cascade = CascadeType.ALL,
             mappedBy = "categoria")
     private List<Producto> productos = new ArrayList<>();
+
 
 }
